5db73caa64bfd3287c271e75dea55ece9fd799b7
raghujay1969-lab <raghujay1969@gmail.com>
2026-05-18 15:24:12 +0530

    Create new.py

---
+++ new.py
@@ -0,0 +1,15 @@
+def linear_search(arr, target):
+      for i in range(len(arr)):
+          if arr[i] == target:
+              return i
+      return -1
+  
+arr = ['Rice','Sugar','Milk','Oil','Soap']
+target = 'Milk'
+result = linear_search(arr, target)
+  
+if result != -1:
+    print(f"Product found at index {result}")
+else:
+    print("Product not found")
+  
